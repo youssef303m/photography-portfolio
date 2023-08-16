0e3ba86884856e1f34cdc42966e46d73f2cc5037
Youssef303M <youssef303m@gmail.com>
2023-08-16 13:56:44 +0300

    Folder and file structure & routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+// import components
+import Header from "./components/Header";
+import AnimRoutes from "./components/AnimRoutes";
+// import router
+import { BrowserRouter as Router } from "react-router-dom";
+// import motion
+import { motion } from "framer-motion";
+
 function App() {
-  return <>Hello World!</>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <AnimRoutes />
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AnimRoutes.jsx
@@ -0,0 +1,20 @@
+// import pages
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Portfolio from "../pages/Portfolio";
+import Contact from "../pages/Contact";
+// import routes, route & useLocation hook
+import { Routes, Route, useLocation } from "react-router-dom";
+
+const AnimRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+};
+
+export default AnimRoutes;
