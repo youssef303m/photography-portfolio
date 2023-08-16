c9e95a60726d817cacbe3dcdbf1f793b45b47b2a
Youssef303M <youssef303m@gmail.com>
2023-08-16 14:43:48 +0300

    Finished home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,36 @@
+// import images
+import WomanImg from "../img/home/woman.png";
+// import link
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <main className="section bg-blue-200">Home</main>;
+  return (
+    <main className="section">
+      <div className="container mx-auto h-full relative">
+        {/* text & img wrapper */}
+        <div className="flex flex-col justify-center">
+          {/* text */}
+          <div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+            <h1 className="h1">
+              photographer <br /> & film maker
+            </h1>
+            <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
+              Log Angeles, USA
+            </p>
+            <Link to={"/contact"} className="btn mb-[30px]">
+              hire me
+            </Link>
+          </div>
+          {/* image */}
+          <div className="flex justify-end lg:max-h-max">
+            <div className="relative lg:-right-40 overflow-hidden">
+              <img src={WomanImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
