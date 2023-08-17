684b68028f34e9c63ead05c4085ffe5bb72c2a81
Youssef303M <youssef303m@gmail.com>
2023-08-17 05:47:11 +0300

    Finished portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,72 @@
+// import images
+import Image1 from "../img/portfolio/1.png";
+import Image2 from "../img/portfolio/2.png";
+import Image3 from "../img/portfolio/3.png";
+import Image4 from "../img/portfolio/4.png";
+// import link
+import { Link } from "react-router-dom";
+
 const Portfolio = () => {
-  return <section className="section bg-green-200">Portfolio</section>;
+  return (
+    <section className="section">
+      <div className="container mx-auto h-full relative">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8">
+          {/* text */}
+          <div className="flex flex-col lg:items-start">
+            <h1 className="h1">Portfolio</h1>
+            <p className="mb-12 max-w-sm text-[#666]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+              <b>Ratione temporibus</b> mollitia vel voluptatem tempore ut amet
+              adipisci deserunt cum optio ullam fugit excepturi modi sequi,
+              quisquam distinctio illo.
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus
+              deleniti quas expedita dolore sunt tempora.
+            </p>
+            <Link to="/contact" className="btn mb-[30px] mx-auto lg:mx-0">
+              Hire me
+            </Link>
+          </div>
+          {/* image grid */}
+          <div className="grid grid-cols-2 lg:gap-2">
+            {/* image */}
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image1}
+                alt=""
+              />
+            </div>
+            {/* image */}
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image2}
+                alt=""
+              />
+            </div>
+            {/* image */}
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image3}
+                alt=""
+              />
+            </div>
+            {/* image */}
+            <div className="max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px] bg-accent overflow-hidden">
+              <img
+                className="object-cover h-full lg:h-[220px] hover:scale-110 transition-all duration-500"
+                src={Image4}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
