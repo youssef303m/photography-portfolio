20527ec8804877c7a512bf22c8f2dbce7d057ddd
Youssef303M <youssef303m@gmail.com>
2023-08-17 05:58:20 +0300

    Finished contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,49 @@
+// import images
+import WomanImg from "../img/contact/woman.png";
+
 const Contact = () => {
-  return <section className="section bg-amber-600">Contact</section>;
+  return (
+    <section className="section">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-start pt-36 gap-x-8 text-center lg:text-left">
+          {/* bg */}
+          <div className="hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 right-0 top-72 -z-10" />
+          {/* text & form */}
+          <div className="lg:flex-1 lg:pt-32 px-4">
+            <h1 className="h1">Contact me</h1>
+            <p className="mb-12">I would love to get suggestions from you.</p>
+            {/* form */}
+            <form className="flex flex-col gap-y-4">
+              <div className="flex gap-x-10">
+                <input
+                  className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]"
+                  type="text"
+                  placeholder="Full Name"
+                />
+                <input
+                  className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]"
+                  type="text"
+                  placeholder="Email Address"
+                />
+              </div>
+              <input
+                className="outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]"
+                type="text"
+                placeholder="Message"
+              />
+              <button className="btn mb-[30px] mx-auto lg:mx-0 self-start">
+                Send it
+              </button>
+            </form>
+          </div>
+          {/* image */}
+          <div className="lg:flex-1">
+            <img src={WomanImg} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
